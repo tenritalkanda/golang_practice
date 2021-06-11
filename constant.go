53a85eec6412ade63c58c54c09f3b0fe3f600157
talkanda <talkanda@gmail.com>
2021-06-11 19:38:06 +0700

    Constant same like php const

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const mobil string = "Honda"
+	const mobil2 = "Toyota"
+
+	const (
+		mobil3 = "Susuki"
+		mobil4 = "Nissan"
+	)
+
+	fmt.Println(mobil, mobil2, mobil3, mobil4)
+}
