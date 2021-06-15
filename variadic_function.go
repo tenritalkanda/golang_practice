9f4cd5456195a07217a82200b62ca48a0fce878d
talkanda <talkanda@gmail.com>
2021-06-15 22:20:52 +0700

    variadic function

---
+++ variadic_function.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	numbers := []int{10, 20, 30, 40, 50}
+	max, _ := variadic("max", numbers...)
+	fmt.Println(max)
+
+	min, _ := variadic("min", 15, 25, 35, 45, 55)
+	fmt.Println(min)
+}
+
+//numbers ...int adalah varargs harus ditaro dipaling kanan (final argument)
+func variadic(operand string, numbers ...int) (int, error) {
+	switch operand {
+	case "max":
+		return max(numbers...), nil
+	case "min":
+		return min(numbers...), nil
+	default:
+		return 0, errors.New("Error, operand not available")
+	}
+}
+
+func max(numbers ...int) (maximum int) {
+	for i, number := range numbers {
+		if i == 0 {
+			maximum = number
+		}
+		if maximum <= number {
+			maximum = number
+		}
+	}
+
+	return
+}
+
+func min(numbers ...int) (minimum int) {
+	for i, number := range numbers {
+		if i == 0 {
+			minimum = number
+		}
+		if minimum >= number {
+			minimum = number
+		}
+	}
+
+	return
+}
